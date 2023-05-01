967edfe720a484047e880fd83426d4da02524566
John Doe <mohaiminur24@gmail.com>
2023-05-01 13:25:37 +0600

    sign in with social media icon is complete

--- src/Components/Pages/LoginRegistrationPage/SignInLayout.jsx
+++ src/Components/Pages/LoginRegistrationPage/SignInLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaFacebook, FaGoogle } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import SigninSocialmedia from './SigninSocialmedia';
 
 
 const SignInLayout = () => {
@@ -19,16 +19,7 @@ const SignInLayout = () => {
                 <p className='font-semibold text-sm'>Don't Have an Account? <NavLink to="/login/registration"><button className='text-yellow-500'>Create an Account</button></NavLink></p>
             </form>
 
-            <div className='flex gap-5 my-5 justify-center items-center'>
-                <hr className='w-full' />
-                <span className='w-fit font-bold'>Or</span>
-                <hr className='w-full' />
-            </div>
-
-            <div>
-                <button className='px-10 block w-3/5 mx-auto my-2 border py-2 rounded-2xl font-semibold relative'> <FaFacebook className="absolute text-blue-800 left-2 top-3" /> Continue with Facebook</button>
-                <button className='px-10 block w-3/5 mx-auto my-2 border py-2 rounded-2xl font-semibold relative'> <FaGoogle className="absolute text-green-500 left-2 top-3" /> Continue with Google</button>
-            </div>
+            <SigninSocialmedia/>
             
         </div>
     );
--- src/Components/Pages/LoginRegistrationPage/SignUpLayout.jsx
+++ src/Components/Pages/LoginRegistrationPage/SignUpLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaFacebook, FaGoogle } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import SigninSocialmedia from './SigninSocialmedia';
 
 const SignUpLayout = () => {
     return (
@@ -20,17 +21,8 @@ const SignUpLayout = () => {
 
                 <p className='font-semibold text-sm'>Already have an Account? <NavLink to="/login"><button className='text-yellow-500'>Login</button></NavLink></p>
             </form>
-
-            <div className='flex gap-5 my-5 justify-center items-center'>
-                <hr className='w-full' />
-                <span className='w-fit font-bold'>Or</span>
-                <hr className='w-full' />
-            </div>
-
-            <div>
-                <button className='px-10 block w-3/5 mx-auto my-2 border py-2 rounded-2xl font-semibold relative'> <FaFacebook className="absolute text-blue-800 left-2 top-3" /> Continue with Facebook</button>
-                <button className='px-10 block w-3/5 mx-auto my-2 border py-2 rounded-2xl font-semibold relative'> <FaGoogle className="absolute text-green-500 left-2 top-3" /> Continue with Google</button>
-            </div>
+            <SigninSocialmedia/>
+            
         </div>
     );
 };
